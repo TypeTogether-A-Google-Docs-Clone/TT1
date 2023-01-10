ce83724121fc7d0913bed03e96cf5187eb73bd21
Kash Elcock <116089696+KashElcock@users.noreply.github.com>
2023-01-10 15:49:28 -0500

    Revert "Added with homepage texteditor"

--- src/main/java/com/typetogether/kash/service/dto/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Data Transfer Objects.
- */
-package com.typetogether.kash.service.dto;
--- src/main/java/com/typetogether/kash/service/mapper/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * MapStruct mappers for mapping domain objects and Data Transfer Objects.
- */
-package com.typetogether.kash.service.mapper;
