a396a74771b714114a1d51933f2e3563d0eb0e53
Hanquan Liu <hanquanl0115@gmail.com>
2023-01-11 16:57:34 -0500

    working on creating documents from homapage

--- src/main/webapp/app/modules/home/home.tsx
+++ src/main/webapp/app/modules/home/home.tsx
@@ -28,6 +28,7 @@ export const Home = () => {
 
   const documentEntity = useAppSelector(state => state.document.entity);
   const loading = useAppSelector(state => state.document.loading);
+  const updating = useAppSelector(state => state.document.updating);
 
   useEffect(() => {
     dispatch(getEntities({}));
@@ -72,6 +73,9 @@ export const Home = () => {
           modifiedDate: convertDateTimeFromServer(documentEntity.modifiedDate),
           user: documentEntity?.user?.id,
         };
+  const handleClose = () => {
+    navigate('/document');
+  };
 
   return (
     <div>
@@ -110,54 +114,29 @@ export const Home = () => {
               </Alert>
             </div>
           )}
-          {/*           <p>If you have any question on JHipster:</p> */}
-
-          {/*           <ul> */}
-          {/*             <li> */}
-          {/*               <a href="https://www.jhipster.tech/" target="_blank" rel="noopener noreferrer"> */}
-          {/*                 JHipster homepage */}
-          {/*               </a> */}
-          {/*             </li> */}
-          {/*             <li> */}
-          {/*               <a href="https://stackoverflow.com/tags/jhipster/info" target="_blank" rel="noopener noreferrer"> */}
-          {/*                 JHipster on Stack Overflow */}
-          {/*               </a> */}
-          {/*             </li> */}
-          {/*             <li> */}
-          {/*               <a href="https://github.com/jhipster/generator-jhipster/issues?state=open" target="_blank" rel="noopener noreferrer"> */}
-          {/*                 JHipster bug tracker */}
-          {/*               </a> */}
-          {/*             </li> */}
-          {/*             <li> */}
-          {/*               <a href="https://gitter.im/jhipster/generator-jhipster" target="_blank" rel="noopener noreferrer"> */}
-          {/*                 JHipster public chat room */}
-          {/*               </a> */}
-          {/*             </li> */}
-          {/*             <li> */}
-          {/*               <a href="https://twitter.com/jhipster" target="_blank" rel="noopener noreferrer"> */}
-          {/*                 follow @jhipster on Twitter */}
-          {/*               </a> */}
-          {/*             </li> */}
-          {/*           </ul> */}
-
-          {/*           <p> */}
-          {/*             If you like JHipster, don&apos;t forget to give us a star on{' '} */}
-          {/*             <a href="https://github.com/jhipster/generator-jhipster" target="_blank" rel="noopener noreferrer"> */}
-          {/*               GitHub */}
-          {/*             </a> */}
-          {/*             ! */}
-          {/*           </p> */}
         </Col>
       </Row>
-      <div className="d-flex justify-content-end">
-        <Link to="/document/new" className="btn btn-primary jh-create-entity" id="jh-create-entity" data-cy="entityCreateButton">
-          <FontAwesomeIcon icon="plus" />
-          &nbsp; Create a new Document
-        </Link>
+      <div className="buttons">
+        <div className="managementbutton">
+          <Link to="/document" className="btn btn-primary jh-create-entity" id="jh-create-entity" data-cy="entityCreateButton">
+            <FontAwesomeIcon icon="plus" />
+            &nbsp; Document Management
+          </Link>
+        </div>
+        <div className="createbutton">
+          {/*         <Link to="/document/new" className="btn btn-success jh-create-entity" id="jh-save-entity" data-cy="entityCreateButton"> */}
+          {/*           <FontAwesomeIcon icon="save" /> */}
+          {/*           &nbsp; Save Current Document */}
+          {/*         </Link> */}
+          <Button color="success" id="save-entity" data-cy="entityCreateSaveButton" type="submit" disabled={updating}>
+            <FontAwesomeIcon icon="save" />
+            &nbsp; Save Current Document
+          </Button>
+        </div>
       </div>
-      <ValidatedForm defaultValues={defaultValues()} onSubmit={saveEntity}>
+      <ValidatedForm defaultValues={defaultValues()} onSubmit={createEntity}>
         <ValidatedField
-          label="Document Title"
+          label="Document Title (* required)"
           id="document-documentTitle"
           name="documentTitle"
           data-cy="documentTitle"
@@ -209,62 +188,3 @@ export const Home = () => {
 };
 
 export default Home;
-
-// // export default function App() {
-//   const editorRef = useRef(null);
-//   const log = () => {
-//     if (editorRef.current) {
-//       console.log(editorRef.current.getContent());
-//     }
-//   };
-//   return (
-//     <>
-//       <Editor
-//         apiKey='pc7rqzul9mdcfrch6wdkvminyzqgq5isq7dd7jj5pdikjwnb'
-//         onInit={(evt, editor) => editorRef.current = editor}
-//         initialValue="<p>This is the initial content of the editor.</p>"
-//         init={{
-//           height: 500,
-//           menubar: false,
-//           plugins: [
-//             'advlist', 'autolink', 'lists', 'link', 'image', 'charmap', 'preview',
-//             'anchor', 'searchreplace', 'visualblocks', 'code', 'fullscreen',
-//             'insertdatetime', 'media', 'table', 'code', 'help', 'wordcount'
-//           ],
-//           toolbar: 'undo redo | blocks | ' +
-//             'bold italic forecolor | alignleft aligncenter ' +
-//             'alignright alignjustify | bullist numlist outdent indent | ' +
-//             'removeformat | help',
-//           content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
-//         }}
-//       />
-//       <button onClick={log}>Log editor content</button>
-//     </>
-//   );
-// //   return (
-//   <head>
-//     <script src="https://cdn.tiny.cloud/1/pc7rqzul9mdcfrch6wdkvminyzqgq5isq7dd7jj5pdikjwnb/tinymce/6/tinymce.min.js" referrerpolicy="origin"></script>
-//   </head>
-//   <body>
-//     <textarea>
-//       Welcome to TinyMCE!
-//     </textarea>
-//     <script>
-//       tinymce.init({
-//         selector: 'textarea',
-//         plugins: 'anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed linkchecker a11ychecker tinymcespellchecker permanentpen powerpaste advtable advcode editimage tinycomments tableofcontents footnotes mergetags autocorrect typography inlinecss',
-//         toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
-//         tinycomments_mode: 'embedded',
-//         tinycomments_author: 'Author name',
-//         mergetags_list: [
-//           { value: 'First.Name', title: 'First Name' },
-//           { value: 'Email', title: 'Email' },
-//         ],
-//       });
-//     </script>
-//   </body>
-//   </html>
-// import { useRef } from 'react';
-// import { Editor } from '@tinymce/tinymce-react';
-
-// export const Home = () => {
